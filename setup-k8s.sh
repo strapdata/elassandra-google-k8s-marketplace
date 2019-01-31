5f9090e14ff31486542b1b8372e4fb3f24c70561
DBarthe <barthelemy.delemotte@gmail.com>
2019-01-31 15:22:51 +0100

    wip

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -3,13 +3,15 @@
 # This script is intended to be used for internal testing only, to create the artifacts necessary for 
 # testing and deploying this code in a sample GKE cluster.
 #
+set -x
+
 PROJECT=${PROJECT:-strapdata-factory}
 CLUSTER=${CLUSTER:-lab}
 ZONE=${ZONE:-europe-west1-b}
 NODES=${NODES:-3}
-API=${API:-beta}
 
-gcloud beta container clusters create $CLUSTER \
+echo "gke: creating cluster"
+gcloud container clusters create $CLUSTER \
     --zone "$ZONE" \
     --project $PROJECT \
     --machine-type "n1-standard-2" \
@@ -17,23 +19,32 @@ gcloud beta container clusters create $CLUSTER \
 #    --max-nodes "6" \
 #    --enable-autoscaling
     
+
+echo "gke: getting credentials"
 gcloud container clusters get-credentials $CLUSTER \
    --zone $ZONE \
    --project $PROJECT
 
 # Configure local auth of docker so that we can use regular
 # docker commands to push/pull from our GCR setup.
+echo "gke: authenticate to gcr"
 gcloud auth configure-docker
 
 # Bootstrap RBAC cluster-admin for your user.
 # More info: https://cloud.google.com/kubernetes-engine/docs/how-to/role-based-access-control
+echo "gke: bootstrap RBAC"
 kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole cluster-admin --user $(gcloud config get-value account)
 
+echo "gke: starting proxy"
 exec nohup kubectl proxy &
 
 # Create google-specific custom resources in the cluster.
+echo "gke: create google app crd"
 kubectl apply -f "https://raw.githubusercontent.com/GoogleCloudPlatform/marketplace-k8s-app-tools/master/crd/app-crd.yaml"
+
+
+echo "gke: cluster created"
 
 # TO DELETE:
 # gcloud beta container clusters delete lab --region europe-west1-b
